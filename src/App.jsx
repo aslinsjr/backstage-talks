ef0f8c807072184b53b8ecb2114d4fda198af9af
Alexandre Lins <aslinsjr@gmail.com>
2024-10-12 22:56:33 -0300

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,63 @@
+import { useState, useEffect } from 'react'
+
+import './App.css'
+
+function App() {
+  const [data, setData] = useState(["blue", "brown", "grey", "light-blue", "pink"])
+  
+  const [windowY, setWindowY] = useState()
+
+  function bgChanger() {
+    const screenHight = window.innerHeight
+
+    console.log(screenHight)
+
+    const body = document.querySelector("body")
+
+    if(windowY < screenHight / 2) {
+      body.style.backgroundColor = `var(--blue-bg)`
+    }
+
+    if(windowY > screenHight - 100) {
+      body.style.backgroundColor = `var(--brown-bg)`
+    }
+
+    if(windowY > (screenHight * 2) - 100) {
+      body.style.backgroundColor = `var(--grey-bg)`
+    }
+
+    if(windowY > (screenHight * 3) - 100) {
+      body.style.backgroundColor = `var(--light-blue-bg)`
+    }
+
+    if(windowY > (screenHight * 4) - 100) {
+      body.style.backgroundColor = `var(--pink-bg)`
+    }
+  }
+
+  document.addEventListener('scroll', () => {
+    setWindowY(window.scrollY)
+  })
+
+  useEffect(() => {
+
+    console.log(windowY)
+
+    bgChanger()
+
+  }, [windowY])
+
+  return (
+    <div className="app">
+      <h1>The book's on the table</h1>
+      {data.map((section) => {
+        return (
+        <section id={section}>
+          <img src={section + "-book.png"} alt={section.toUpperCase() + " Image Book"} />
+        </section>
+      )})}
+    </div>
+  )
+}
+
+export default App
